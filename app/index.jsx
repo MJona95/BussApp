2f5460c033ebe62e95c68c590a9ed1945cdd569d
Marvin Gutierrez <mjona9514@gmail.com>
2025-01-30 21:17:29 -0600

    create the count of the days

--- app/index.jsx
+++ app/index.jsx
@@ -14,40 +14,74 @@ import { dataL } from '../components/dataL';
 import { useRef, useCallback, useState, useEffect } from 'react';
 
 import { useFocusEffect } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function home() {
 
   const [ Ldata, setLdata ] = useState([]);
-
   const [ Fdata, setFdata ] = useState([]);
+  const [ Cards, setCards ] = useState([]);
+  const [valueDate, setValueDate] = useState(null);
 
   useEffect(() => {
 
     const fetchData = async () => {
       try {
         
-        // Obtén la colección 'indexcards' desde Firestore
-      const querySnapshot = await getDocs(collection(database, 'indexcards'));
+        //Se busca una variable llamada dateLastFetching y su valor se guarda en Date
+        const storageDate = await AsyncStorage.getItem('dateLastFetching')
+        //setValueDate se cambia de null al valor guardato previamente en Date
+        setValueDate(storageDate)
+        /*Se hace una comparacion del valor de valueDate si es diferente a null significa
+        que hay un valor guardado de la ultima vez que se hicieron cambios en dicha variable*/
+        if(valueDate !== null) {
 
-      // Mapeamos los datos para extraer únicamente el array de objetos `indexcards`
-      const indexCard = querySnapshot.docs.flatMap(doc => {
-        const data = doc.data(); // Obtén los datos del documento
-        return data.indexcards || []; // Retorna el array `indexcards` o un array vacío si no existe
-      });
+          const lastDate = new Date(valueDate);
+          const today = new Date()
+          const todayFormatt = new Date(today.toISOString().split('T')[0]);
+
+          const diffTime = todayFormatt - lastDate
+          const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); // Diferencia en días
+
+          
+          console.log(`han transcurrido ${diffDays} dias `)
+          //Comparacion de la ultima fecha con la fecha actual para saber si ya pasaron 90 dias
+          console.log(valueDate+' es el primer dia');
+
+          
+        } else {
+          const toDay = new Date().toISOString().split('T')[0];
+          const Date = await AsyncStorage.setItem('dateLastFetching', toDay)
+          setValueDate(Date)
+          console.log(valueDate)
+        }
+
+        // Obtén la colección 'indexcards' desde Firestore
+        const querySnapshot = await getDocs(collection(database, 'indexcards'));
+
+        // Mapeamos los datos para extraer únicamente el array de objetos `indexcards`
+        const indexCard = querySnapshot.docs.flatMap(doc => {
+          const data = doc.data(); // Obtén los datos del documento
+          return data.indexcards || []; // Retorna el array `indexcards` o un array vacío si no existe
+        });
 
         setFdata(indexCard)
-        console.log(setFdata)
 
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
+    
+
     fetchData();
 
     setLdata(dataL[0])
-    console.log("");
-    console.log(setLdata)
+
+    Fdata != [] ? setCards(Fdata) : setCards(Ldata)
+
+    console.log('====================================================================================')
+    console.log(Fdata)
 
   }, []);
 
@@ -61,6 +95,8 @@ export default function home() {
       }
     }, [])
   );
+
+
 
   return (
     <ScrollView style={styles.container}>
@@ -81,7 +117,7 @@ export default function home() {
 
             {
 
-              Fdata.map((info, index) => <Card key={index} image={info.image} icon={info.iconh} title={info.title} texto={info.texto} spam={info.spam} iconf={info.iconf} modal={info.modal} />)
+              Cards.map((info, index) => <Card key={index} image={info.image} icon={info.iconh} title={info.title} texto={info.texto} spam={info.spam} iconf={info.iconf} modal={info.modal} />)
 
             }
             
