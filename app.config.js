ad66532010b9a383cd1beb5313c93621acc3de1c
Marvin Gutierrez <mjona9514@gmail.com>
2025-02-26 13:48:42 -0600

    last build

--- app.config.js
+++ app.config.js
@@ -8,16 +8,23 @@ export default {
     "slug": "BussApp",
     "version": "1.0.0",
     "orientation": "portrait",
+    "icon": "./assets/icon.png",
     "scheme": "whatsapp",
     "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
     ios: {
       "supportsTablet": true
     },
     android: {
       package: "com.Mjona9514.tuapp",
-    },
-    web: {
-      "favicon": "./assets/favicon.png"
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
     },
     owner: "mjona9514",
     plugins: [
